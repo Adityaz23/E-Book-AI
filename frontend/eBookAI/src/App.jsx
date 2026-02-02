a3c4108a6d116e705a2a253180978e1e6b727572
Adityaz23 <soniadityakumar651.com>
2026-02-02 13:19:12 +0530

    Starting of the E-Book AI application

---
+++ frontend/eBookAI/src/App.jsx
@@ -0,0 +1,9 @@
+function App () {
+  return(
+    <>
+    <div className="text-5xl font-bold">Hello from the frontend.</div>
+    <div className="text-4xl font-extrabold">From the ebook</div>
+    </>
+  )
+}
+export default App
